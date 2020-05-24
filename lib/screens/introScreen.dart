519fec4fd5c4a14a45278c085f88b32f8d1c9103
Rahul_Vijay <vk.rahul318@gmail.com>
2020-05-24 14:54:11 +0530

    added first graphic

--- lib/screens/introScreen.dart
+++ lib/screens/introScreen.dart
@@ -1,18 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:mvp/constants/themeColours.dart';
+import 'package:mvp/graphics/Intro_greenTopLeft.dart';
 
 class IntroScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Column(
+        child: Stack(
           children: <Widget>[
-            Text(
-              "Seva",
-              style: TextStyle(
-                  color: ThemeColoursSeva().lgGreen,
-                  fontSize: 45.0,),
+            CustomPaint(
+              painter: GreenPaintTopLeft(),
+              child: Container(
+                child: Center(
+                  child: Text("SEVA")
+                ),
+              ),
             ),
           ],
         ),
