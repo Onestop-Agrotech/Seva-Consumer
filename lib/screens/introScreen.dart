d1f471b2fc64951888f3670fd907e865f501d2fe
Rahul_Vijay <vk.rahul318@gmail.com>
2020-05-24 16:13:13 +0530

    image and intro pages

--- lib/screens/introScreen.dart
+++ lib/screens/introScreen.dart
@@ -5,6 +5,8 @@ import 'package:mvp/graphics/greenIntro.dart';
 class IntroScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    double sWidth = MediaQuery.of(context).size.width;
+    double sHeight = MediaQuery.of(context).size.height;
     return Scaffold(
       body: SafeArea(
         child: Stack(
@@ -43,6 +45,42 @@ class IntroScreen extends StatelessWidget {
                   ),
                 ),
                 top: 120),
+            Positioned(
+              left: sWidth * 0.075,
+              top: sHeight * 0.32,
+              child: Container(
+                height: sHeight * 0.58,
+                width: sWidth * 0.85,
+                child: Column(
+                  children: <Widget>[
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: <Widget>[
+                        Image(
+                          image: AssetImage('images/person.png'),
+                        ),
+                        Image(
+                          image: AssetImage('images/mob.png'),
+                        ),
+                      ],
+                    ),
+                    SizedBox(
+                      height: 10.0,
+                    ),
+                    Container(
+                      width: sWidth * 0.7,
+                      child: Text(
+                        "Order Fresh Fruits and Vegetables from the comfort of your home. We collect from farmers driectly!",
+                        style: TextStyle(
+                            fontSize: 19,
+                            fontFamily: "Raleway",
+                            color: ThemeColoursSeva().dkGreen),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
           ],
         ),
       ),
