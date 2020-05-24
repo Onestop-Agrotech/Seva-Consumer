d0dec9df7a267a2e993392c26d163ebe8e7c73f7
Rahul_Vijay <vk.rahul318@gmail.com>
2020-05-24 15:50:01 +0530

    add new font

--- lib/screens/introScreen.dart
+++ lib/screens/introScreen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:mvp/constants/themeColours.dart';
 import 'package:mvp/graphics/greenIntro.dart';
 
 class IntroScreen extends StatelessWidget {
@@ -14,7 +15,34 @@ class IntroScreen extends StatelessWidget {
                 child: null,
               ),
             ),
-            
+            Positioned.fill(
+                child: Align(
+                  alignment: Alignment.topCenter,
+                  child: Column(
+                    children: <Widget>[
+                      Text(
+                        "SEVA",
+                        style: TextStyle(
+                            fontSize: 45.0,
+                            color: ThemeColoursSeva().lgGreen,
+                            fontFamily: "Raleway",
+                            fontWeight: FontWeight.w700),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(left: 60.0),
+                        child: Text(
+                          "By ONESTOP",
+                          style: TextStyle(
+                              fontFamily: "Raleway",
+                              fontWeight: FontWeight.w300,
+                              color: ThemeColoursSeva().lgGreen,
+                              fontSize: 16.0),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                top: 120),
           ],
         ),
       ),
