670e7c2afcf1f38931f4f264a1d70ee6830aea0a
Rahul_Vijay <vk.rahul318@gmail.com>
2020-05-26 14:09:08 +0530

    added diff options

--- lib/screens/products.dart
+++ lib/screens/products.dart
@@ -1,7 +1,10 @@
 import 'package:flappy_search_bar/flappy_search_bar.dart';
 import 'package:flutter/material.dart';
+import 'package:mvp/constants/themeColours.dart';
 import 'package:mvp/graphics/greenProducts.dart';
 import 'package:mvp/screens/common/topText.dart';
+
+import 'common/smallDotsIntro.dart';
 
 class ProductScreen extends StatefulWidget {
   @override
@@ -33,7 +36,44 @@ class _ProductScreenState extends State<ProductScreen> {
                   children: <Widget>[
                     SizedBox(height: 20.0),
                     TopText(txt: "Products"),
-                    SizedBox(height: 20.0),
+                    SizedBox(height: 120.0),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: <Widget>[
+                        Column(
+                          children: <Widget>[
+                            Text(
+                              'Vegetables',
+                              style: TextStyle(
+                                  color: ThemeColoursSeva().dkGreen,
+                                  fontFamily: "Raleway",
+                                  fontWeight: FontWeight.w500,
+                                  fontSize: 20.0),
+                            ),
+                            SizedBox(
+                              height: 18.0,
+                            ),
+                            SmallDotsIntro(bg: ThemeColoursSeva().black),
+                          ],
+                        ),
+                        Column(
+                          children: <Widget>[
+                            Text(
+                              'Fruits',
+                              style: TextStyle(
+                                  color: Colors.grey,
+                                  fontFamily: "Raleway",
+                                  fontWeight: FontWeight.w500,
+                                  fontSize: 20.0),
+                            ),
+                            SizedBox(
+                              height: 18.0,
+                            ),
+                            SmallDotsIntro(bg: ThemeColoursSeva().grey),
+                          ],
+                        ),
+                      ],
+                    ),
                   ],
                 ),
               ),
@@ -42,7 +82,10 @@ class _ProductScreenState extends State<ProductScreen> {
           SafeArea(
             child: Padding(
               padding: const EdgeInsets.only(left: 20, right: 20, top: 70),
-              child: SearchBar(onItemFound: null, onSearch: null,),
+              child: SearchBar(
+                onItemFound: null,
+                onSearch: null,
+              ),
             ),
           ),
         ],
