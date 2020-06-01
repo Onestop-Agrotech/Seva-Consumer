0b75d95d808ff2805fc489f99d010ce66a98297b
Rahul_Vijay <vk.rahul318@gmail.com>
2020-06-01 16:03:09 +0530

    add firebase & revamp

--- lib/screens/products.dart
+++ lib/screens/products.dart
@@ -5,44 +5,13 @@ import 'package:mvp/graphics/greenProducts.dart';
 import 'package:mvp/screens/common/customProductCard.dart';
 import 'package:mvp/screens/common/topText.dart';
 
-import 'common/smallDotsIntro.dart';
-
 class ProductScreen extends StatefulWidget {
   @override
   _ProductScreenState createState() => _ProductScreenState();
 }
 
 class _ProductScreenState extends State<ProductScreen> {
-  ScrollController _scrollController = ScrollController(); 
   TextEditingController search;
-  int _index = 0;
-
-  _buildRow(int indexNumber) {
-    return ListView.builder(
-      controller: _scrollController,
-        itemCount: 10,
-        itemBuilder: (BuildContext ctx, int index) {
-          return Column(
-            children: <Widget>[
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: <Widget>[
-                  indexNumber == 0 ? ProductCard(cc: Colors.lightBlue) : ProductCard(cc: Colors.lightGreen),
-                  indexNumber == 0 ? ProductCard(cc: Colors.lightBlue) : ProductCard(cc: Colors.lightGreen),
-                ],
-              ),
-              SizedBox(height:15.0)
-            ],
-          );
-        });
-  }
-
-  IndexedStack _buildStack() {
-    return IndexedStack(
-      index: _index==0?0:1,
-      children: <Widget>[_buildRow(_index),_buildRow(_index)],
-    );
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -63,14 +32,14 @@ class _ProductScreenState extends State<ProductScreen> {
             child: Align(
               alignment: Alignment.topCenter,
               child: Padding(
-                padding: const EdgeInsets.only(top: 60.0),
-                child: TopText(txt: "Products"),
+                padding: const EdgeInsets.only(top: 60.0, left: 40.0, right: 20.0),
+                child: TopText(txt: "Sri Laxmi Vegetables & fruits",),
               ),
             ),
           ),
           SafeArea(
             child: Padding(
-              padding: const EdgeInsets.only(left: 20, right: 20, top: 70),
+              padding: const EdgeInsets.only(left: 20, right: 20, top: 90),
               child: SearchBar(
                 onItemFound: null,
                 onSearch: null,
@@ -81,86 +50,8 @@ class _ProductScreenState extends State<ProductScreen> {
             child: Align(
               alignment: Alignment.topCenter,
               child: Padding(
-                padding: const EdgeInsets.only(top: 200.0),
-                child: Container(
-                  height: 64.0,
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: <Widget>[
-                      Material(
-                        child: InkWell(
-                          onTap: () {
-                            _scrollController.animateTo(0.0, duration: const Duration(milliseconds: 300), curve: Curves.ease);
-                            setState(() {
-                              _index = 0;
-                            });
-                          },
-                          child: Column(
-                            children: <Widget>[
-                              Text(
-                                'Vegetables',
-                                style: TextStyle(
-                                    color: _index == 0
-                                        ? ThemeColoursSeva().dkGreen
-                                        : Colors.grey,
-                                    fontFamily: "Raleway",
-                                    fontWeight: FontWeight.w500,
-                                    fontSize: 20.0),
-                              ),
-                              SizedBox(
-                                height: 18.0,
-                              ),
-                              SmallDotsIntro(
-                                  bg: _index == 0
-                                      ? ThemeColoursSeva().black
-                                      : ThemeColoursSeva().grey),
-                            ],
-                          ),
-                        ),
-                      ),
-                      Material(
-                        child: InkWell(
-                          onTap: () {
-                            _scrollController.animateTo(0.0, duration: const Duration(milliseconds: 300), curve: Curves.ease);
-                            setState(() {
-                              _index = 1;
-                            });
-                          },
-                          child: Column(
-                            children: <Widget>[
-                              Text(
-                                'Fruits',
-                                style: TextStyle(
-                                    color: _index == 1
-                                        ? ThemeColoursSeva().dkGreen
-                                        : Colors.grey,
-                                    fontFamily: "Raleway",
-                                    fontWeight: FontWeight.w500,
-                                    fontSize: 20.0),
-                              ),
-                              SizedBox(
-                                height: 18.0,
-                              ),
-                              SmallDotsIntro(
-                                  bg: _index == 1
-                                      ? ThemeColoursSeva().black
-                                      : ThemeColoursSeva().grey),
-                            ],
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-            ),
-          ),
-          Positioned.fill(
-            child: Align(
-              alignment: Alignment.topCenter,
-              child: Padding(
-                padding: const EdgeInsets.only(top: 265.0),
-                child: _buildStack(),
+                padding: const EdgeInsets.only(top: 210.0),
+                child: Text("Hello"),
               ),
             ),
           ),
