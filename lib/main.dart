714e6d4612a2c8c35a974311db67f665fd4a2622
Rahul_Vijay <vk.rahul318@gmail.com>
2020-06-04 13:44:18 +0530

    map play around

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:mvp/models/cart.dart';
+// import 'package:mvp/models/cart.dart';
 import 'package:mvp/screens/location.dart';
 // import 'package:mvp/screens/auth/login.dart';
 // import 'package:mvp/screens/introScreen.dart';
 // import 'package:mvp/screens/products.dart';
-import 'package:mvp/screens/storesList.dart';
-import 'package:provider/provider.dart';
+// import 'package:mvp/screens/storesList.dart';
+// import 'package:provider/provider.dart';
 
 void main() {
   runApp(SevaApp());
--- lib/screens/location.dart
+++ lib/screens/location.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
-import 'package:flappy_search_bar/flappy_search_bar.dart';
 
 class GoogleLocationScreen extends StatefulWidget {
   @override
@@ -8,24 +7,43 @@ class GoogleLocationScreen extends StatefulWidget {
 }
 
 class _GoogleLocationScreenState extends State<GoogleLocationScreen> {
-
   GoogleMapController mapController;
-  final LatLng _center = const LatLng(12.962579, 77.497794);
+  TextEditingController _searchControl = new TextEditingController();
+
+  final LatLng _center = const LatLng(28.570860, 77.368949);
   Set<Marker> _markers = {};
+  LatLng home;
 
   @override
   void initState() {
     super.initState();
     _markers.add(Marker(
-      markerId: MarkerId('v bro'),
-      position: _center,
-      draggable: true
-    ));
+        markerId: MarkerId('v bro'),
+        position: _center,
+        draggable: true,
+        onDragEnd: ((value) {
+          print(value.latitude);
+          print(value.longitude);
+        })));
   }
 
   void _onMapCreated(GoogleMapController controller) {
     mapController = controller;
+    // _encodeLocation();
   }
+
+
+  // _onSearchHandler() async{
+  //   // _searchQueryController
+  //   String q = _searchControl.value.toString();
+  //   var addresses = await Geocoder.local.findAddressesFromQuery(q);
+  //   var first = addresses.first;
+  //   LatLng coords =
+  //       LatLng(first.coordinates.latitude, first.coordinates.longitude);
+  //   mapController.animateCamera(CameraUpdate.newCameraPosition(
+  //     CameraPosition(target: coords, zoom:15.0)
+  //   ));
+  // }
 
   @override
   Widget build(BuildContext context) {
@@ -35,11 +53,48 @@ class _GoogleLocationScreenState extends State<GoogleLocationScreen> {
         children: <Widget>[
           GoogleMap(
             onMapCreated: _onMapCreated,
-            initialCameraPosition: CameraPosition(target: _center, zoom: 16.0),
+            initialCameraPosition: CameraPosition(target: _center, zoom: 17.0),
             markers: Set.from(_markers),
+          ),
+          SafeArea(
+            child: Padding(
+              padding: const EdgeInsets.only(left: 20, right: 20, top: 30),
+              child: Container(
+                height: 60.0,
+                width: double.infinity,
+                decoration: BoxDecoration(
+                    color: Color(0xffebedf0),
+                    borderRadius: BorderRadius.circular(20.0)),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: <Widget>[
+                    SizedBox(width: 5.0),
+                    IconButton(icon: Icon(Icons.search), onPressed:null),
+                    SizedBox(width: 5.0),
+                    Container(
+                        width: 270.0,
+                        child: TextFormField(
+                            controller: _searchControl,
+                            decoration: InputDecoration(
+                              border: InputBorder.none,
+                              focusedBorder: InputBorder.none,
+                              enabledBorder: InputBorder.none,
+                              errorBorder: InputBorder.none,
+                              disabledBorder: InputBorder.none,
+                            )))
+                  ],
+                ),
+              ),
+            ),
           ),
         ],
       ),
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: () {},
+        label: Text("Set as Delivery Address"),
+        icon: Icon(Icons.home),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
     );
   }
 }
