d319af9a2414a979a65b3bd6d3741daa6167a536
Rahul_Vijay <vk.rahul318@gmail.com>
2020-06-06 11:37:34 +0530

    check for token validity

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,24 @@
 import 'package:flutter/material.dart';
-import 'package:mvp/screens/auth/login.dart';
-// import 'package:mvp/screens/auth/register.dart';
-// import 'package:mvp/models/cart.dart';
-// import 'package:mvp/screens/location.dart';
-// import 'package:mvp/screens/auth/login.dart';
-// import 'package:mvp/screens/introScreen.dart';
-// import 'package:mvp/screens/products.dart';
-// import 'package:mvp/screens/storesList.dart';
-// import 'package:provider/provider.dart';
+import 'package:mvp/models/cart.dart';
+import 'package:mvp/screens/loading.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(SevaApp());
 }
 
-class SevaApp extends StatelessWidget {
+class SevaApp extends StatefulWidget {
+  @override
+  _SevaAppState createState() => _SevaAppState();
+}
+
+class _SevaAppState extends State<SevaApp> {
   @override
   Widget build(BuildContext context) {
-    // return MultiProvider(
-    //   providers: [ChangeNotifierProvider(create: (context) => CartModel())],
-    //   child:
-    //       MaterialApp(debugShowCheckedModeBanner: false, home: StoresScreen()),
-    // );
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: LoginScreen(),
+    return MultiProvider(
+      providers: [ChangeNotifierProvider(create: (context) => CartModel())],
+      child:
+          MaterialApp(debugShowCheckedModeBanner: false, home: LoadingScreen()),
     );
   }
 }
