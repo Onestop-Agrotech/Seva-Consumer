b375e3e42b7e66767d167fac3bf582b1e880db97
Rahul_Vijay <vk.rahul318@gmail.com>
2020-06-17 17:18:20 +0530

    fixed size for small screens

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:mvp/models/cart.dart';
 import 'package:mvp/screens/auth/forgotPassword.dart';
 import 'package:mvp/screens/auth/login.dart';
+import 'package:mvp/screens/auth/register.dart';
 import 'package:mvp/screens/loading.dart';
 import 'package:mvp/screens/orders.dart';
 import 'package:mvp/screens/storesList.dart';
@@ -24,6 +25,7 @@ class _SevaAppState extends State<SevaApp> {
       child:
           MaterialApp(debugShowCheckedModeBanner: false, home: LoadingScreen(),
           routes: {
+            "/register": (context) => RegisterScreen(),
             "/login": (context) => LoginScreen(),
             "/orders": (context) => OrdersScreen(),
             "/stores": (context) => StoresScreen(),
