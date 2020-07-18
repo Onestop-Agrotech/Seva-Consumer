6308fa614ca687f641db408d4035f17851ee81b0
Vikas <vikasprashar99@gmail.com>
2020-07-18 22:51:12 +0530

    added auth on apis

--- lib/classes/crud_firestore.dart
+++ lib/classes/crud_firestore.dart
@@ -11,59 +11,43 @@ class FirestoreCRUD {
   void addToFirestore(StoreProduct obj) async {
     StorageSharedPrefs p = new StorageSharedPrefs();
     String id = await p.getId();
-    String url = APIService.setCartAPI ;
-    String body=jsonEncode(
-{
+    String token = await p.getToken();
+    String url = APIService.setCartAPI;
+    Map<String, String> requestHeaders = {'x-auth-token': token};
+    String body = jsonEncode({
       'description': obj.description,
-      'userId':id,
-      'name':obj.name,
-      'pictureURL':obj.pictureUrl,
-      'productPrice':obj.price,
-      'quantityMetric':obj.quantity.quantityMetric,
-      'quantityValue':obj.quantity.quantityValue,
-      'type':obj.type,
-      'uniqueId':obj.uniqueId,
-      'userPrice':obj.totalPrice,
-      'userQuantity':obj.totalQuantity
-      
-    }
-
-    );
-    var response = await http.post(url, body: body);
-  if (response.statusCode == 200) {
+      'userId': id,
+      'name': obj.name,
+      'pictureURL': obj.pictureUrl,
+      'productPrice': obj.price,
+      'quantityMetric': obj.quantity.quantityMetric,
+      'quantityValue': obj.quantity.quantityValue,
+      'type': obj.type,
+      'uniqueId': obj.uniqueId,
+      'userPrice': obj.totalPrice,
+      'userQuantity': obj.totalQuantity
+    });
+    var response = await http.post(url, body: body, headers: requestHeaders);
+    if (response.statusCode == 200) {
       print("success");
     } else {
       throw Exception('something is wrong');
     }
-    // Firestore.instance.collection('$id').document('p-${obj.uniqueId}').setData({
-    //   'name': obj.name,
-    //   'productPrice': obj.price,
-    //   'userQuantity': obj.totalQuantity,
-    //   'userPrice': obj.totalPrice,
-    //   'quantityValue': obj.quantity.quantityValue,
-    //   "quantityMetric": obj.quantity.quantityMetric,
-    //   'uniqueId': obj.uniqueId,
-    //   'description': obj.description,
-    //   'id': obj.id,
-    //   'type': obj.type,
-    //   'pictureURL': obj.pictureUrl
-    // });
   }
 
   // Delete a particular document from firestore
   void deleteFromFirestore(String uId) async {
     StorageSharedPrefs p = new StorageSharedPrefs();
     String id = await p.getId();
-  String url = APIService.removeitemAPI ;
-    String body=jsonEncode(
-    {
-      'userId':id,
-      'uniqueId':uId,
-    }
-
-    );
-    var response = await http.post(url, body: body);
-  if (response.statusCode == 200) {
+    String token = await p.getToken();
+    Map<String, String> requestHeaders = {'x-auth-token': token};
+    String url = APIService.removeitemAPI;
+    String body = jsonEncode({
+      'userId': id,
+      'uniqueId': uId,
+    });
+    var response = await http.post(url, body: body, headers: requestHeaders);
+    if (response.statusCode == 200) {
       print("success");
     } else {
       throw Exception('something is wrong');
@@ -75,18 +59,18 @@ class FirestoreCRUD {
   void updateDocInFirestore(String docId, int newq, int newp) async {
     StorageSharedPrefs p = new StorageSharedPrefs();
     String id = await p.getId();
- String url = APIService.updatecartAPI ;
-    String body=jsonEncode(
-    {
-      'userPrice':newp,
-      'userQuantity':newq,
-      'uniqueId':docId,
-      'userId':id
-    }
+    String token = await p.getToken();
+    String url = APIService.updatecartAPI;
+    Map<String, String> requestHeaders = {'x-auth-token': token};
 
-    );
-    var response = await http.post(url, body: body);
-  if (response.statusCode == 200) {
+    String body = jsonEncode({
+      'userPrice': newp,
+      'userQuantity': newq,
+      'uniqueId': docId,
+      'userId': id
+    });
+    var response = await http.post(url, body: body, headers: requestHeaders);
+    if (response.statusCode == 200) {
       print("success");
     } else {
       throw Exception('something is wrong');
@@ -102,16 +86,14 @@ class FirestoreCRUD {
   void deleteDocuments() async {
     StorageSharedPrefs p = new StorageSharedPrefs();
     String id = await p.getId();
-
-     String url = APIService.emptycartAPI ;
-    String body=jsonEncode(
-    {
-      'userId':id,
-    }
-
-    );
-    var response = await http.post(url, body: body);
-  if (response.statusCode == 200) {
+    String token = await p.getToken();
+    String url = APIService.emptycartAPI;
+    Map<String, String> requestHeaders = {'x-auth-token': token};
+    String body = jsonEncode({
+      'userId': id,
+    });
+    var response = await http.post(url, body: body, headers: requestHeaders);
+    if (response.statusCode == 200) {
       print("success");
     } else {
       throw Exception('something is wrong');
