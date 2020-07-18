a08551617af5d4920e8d2089348e69fc4136e690
Vikas <vikasprashar99@gmail.com>
2020-07-18 22:11:03 +0530

    set item operation added

--- lib/classes/crud_firestore.dart
+++ lib/classes/crud_firestore.dart
@@ -1,5 +1,8 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:mvp/classes/storage_sharedPrefs.dart';
+import 'package:mvp/constants/apiCalls.dart';
 import 'package:mvp/models/storeProducts.dart';
 
 // This class helps to interact with the Firestore Real time DB
@@ -9,19 +12,43 @@ class FirestoreCRUD {
   void addToFirestore(StoreProduct obj) async {
     StorageSharedPrefs p = new StorageSharedPrefs();
     String id = await p.getId();
-    Firestore.instance.collection('$id').document('p-${obj.uniqueId}').setData({
-      'name': obj.name,
-      'productPrice': obj.price,
-      'userQuantity': obj.totalQuantity,
-      'userPrice': obj.totalPrice,
-      'quantityValue': obj.quantity.quantityValue,
-      "quantityMetric": obj.quantity.quantityMetric,
-      'uniqueId': obj.uniqueId,
+    String url = APIService.setCartAPI + "$id";
+    String body=jsonEncode(
+{
       'description': obj.description,
-      'id': obj.id,
-      'type': obj.type,
-      'pictureURL': obj.pictureUrl
-    });
+      'userId':obj.id,
+      'name':obj.name,
+      'pictureURL':obj.pictureUrl,
+      'productPrice':obj.totalPrice,
+      'quantityMetric':obj.quantity.quantityMetric,
+      'quantityValue':obj.quantity.quantityValue,
+      'type':obj.type,
+      'uniqueId':obj.uniqueId,
+      'userPrice':obj.totalPrice,
+      'userQuantity':obj.totalQuantity
+      
+    }
+
+    );
+    var response = await http.post(url, body: body);
+  if (response.statusCode == 200) {
+      print("success");
+    } else {
+      throw Exception('something is wrong');
+    }
+    // Firestore.instance.collection('$id').document('p-${obj.uniqueId}').setData({
+    //   'name': obj.name,
+    //   'productPrice': obj.price,
+    //   'userQuantity': obj.totalQuantity,
+    //   'userPrice': obj.totalPrice,
+    //   'quantityValue': obj.quantity.quantityValue,
+    //   "quantityMetric": obj.quantity.quantityMetric,
+    //   'uniqueId': obj.uniqueId,
+    //   'description': obj.description,
+    //   'id': obj.id,
+    //   'type': obj.type,
+    //   'pictureURL': obj.pictureUrl
+    // });
   }
 
   // Delete a particular document from firestore
