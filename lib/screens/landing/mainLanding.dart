a255125f3b2e203cf09a21d7eb8d11cb385ea4a5
Rahul_Vijay <vk.rahul318@gmail.com>
2020-07-30 15:46:25 +0530

    top bar

--- lib/screens/landing/mainLanding.dart
+++ lib/screens/landing/mainLanding.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:mvp/constants/themeColours.dart';
 import 'package:mvp/screens/landing/graphics/darkBG.dart';
 
 import 'graphics/lightBG.dart';
@@ -11,6 +12,8 @@ class MainLandingScreen extends StatefulWidget {
 class _MainLandingScreenState extends State<MainLandingScreen> {
   @override
   Widget build(BuildContext context) {
+    double height = MediaQuery.of(context).size.height;
+    double width = MediaQuery.of(context).size.width;
     return Scaffold(
       body: Stack(
         children: <Widget>[
@@ -21,7 +24,37 @@ class _MainLandingScreenState extends State<MainLandingScreen> {
           CustomPaint(
             painter: DarkColourBG(),
             child: Container(),
-          )
+          ),
+          Positioned.fill(
+            child: Align(
+              alignment: Alignment.topCenter,
+              child: Padding(
+                padding: const EdgeInsets.only(top: 8.0, left: 8.0, right: 8.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    IconButton(
+                      icon: Icon(Icons.menu),
+                      onPressed: () {},
+                      iconSize: 28.0,
+                    ),
+                    Text(
+                      "Welcome",
+                      style: TextStyle(
+                          color: ThemeColoursSeva().dkGreen,
+                          fontSize: 24.0,
+                          fontWeight: FontWeight.bold),
+                    ),
+                    IconButton(
+                      icon: Icon(Icons.location_on),
+                      onPressed: () {},
+                      iconSize: 28.0,
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
         ],
       ),
     );
