efb4b0a94434ca2836be7317f9d8a62ff442bad2
Vikas Prashar <vikasprashar99@gmail.com>
2020-07-31 22:19:37 +0530

    added modal on click

--- lib/screens/common/productCard.dart
+++ lib/screens/common/productCard.dart
@@ -9,117 +9,7 @@ class ProductCardNew extends StatefulWidget {
   final bool shopping;
   ProductCardNew({this.shopping});
 
-  // onClickProduct() {
-  //   showGeneralDialog(
-  // context: context,
-  // barrierDismissible: true,
-  // barrierLabel:
-  //     MaterialLocalizations.of(context).modalBarrierDismissLabel,
-  // barrierColor: Colors.black45,
-  // transitionDuration: const Duration(milliseconds: 200),
-  // pageBuilder: (BuildContext buildContext, Animation animation,
-  //     Animation secondaryAnimation) {
-  //   return Material(
-  //     type: MaterialType.transparency,
-  //     child: Center(
-  //       child: Container(
-  //         width: MediaQuery.of(context).size.width - 50,
-  //         height: MediaQuery.of(context).size.height - 300,
-  //         padding: EdgeInsets.all(20),
-  //         color: Colors.white,
-  //         child: Column(
-  //           children: [
-  //             RaisedButton(
-  //               onPressed: () {
-  //                 Navigator.of(context).pop();
-  //               },
-  //               child: Text(
-  //                 "Save",
-  //                 style: TextStyle(color: Colors.white),
-  //               ),
-  //               color: const Color(0xFF1BC0C5),
-  //             ),
-  //             Text(
-  //               "Apple - Red Delicious",
-  //               style: TextStyle(
-  //                   color: ThemeColoursSeva().pallete2,
-  //                   fontSize: 25.0,
-  //                   fontWeight: FontWeight.w500),
-  //             ),
-  //             SizedBox(
-  //               height: 10,
-  //             ),
-  //             Text("Rs 120 - 1 Kg",
-  //                 overflow: TextOverflow.clip,
-  //                 style: TextStyle(
-  //                     color: ThemeColoursSeva().pallete2,
-  //                     fontSize: 25.0,
-  //                     fontWeight: FontWeight.w500)),
-  //             SizedBox(height: 10),
-  //             Row(
-  //               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-  //               children: <Widget>[
-  //                 // ConstrainedBox(
-  //                 //     constraints:
-  //                 //         BoxConstraints(minWidth: 600, maxHeight: 160),
-  //                 //     child: CachedNetworkImage(
-  //                 //         imageUrl:
-  //                 //             "https://storepictures.theonestop.co.in/products/pineapple.png")
-  //                 //             ),
-  //                 CachedNetworkImage(
-  //                     width: 200,
-  //                     height: 140,
-  //                     imageUrl:
-  //                         "https://storepictures.theonestop.co.in/products/pineapple.png"),
-  //                 Column(
-  //                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
-  //                   // crossAxisAlignment: CrossAxisAlignment.,
-  //                   children: <Widget>[
-  //                     Text(
-  //                       "200 Gms",
-  //                       style: TextStyle(
-  //                           color: ThemeColoursSeva().pallete2,
-  //                           fontSize: 20.0,
-  //                           fontWeight: FontWeight.w500),
-  //                     ),
-  //                     Text(
-  //                       "200 Gms",
-  //                       style: TextStyle(
-  //                           color: ThemeColoursSeva().pallete2,
-  //                           fontSize: 20.0,
-  //                           fontWeight: FontWeight.w500),
-  //                     ),
-  //                     Text(
-  //                       "200 Gms",
-  //                       style: TextStyle(
-  //                           color: ThemeColoursSeva().pallete2,
-  //                           fontSize: 20.0,
-  //                           fontWeight: FontWeight.w500),
-  //                     ),
-  //                   ],
-  //                 ),
-  //               ],
-  //             ),
-  //             SizedBox(
-  //               height: 10,
-  //             ),
-  //             Text(
-  //                 "Apples contain  no fat, sodium or cholestrol and are a good source"),
-  //                 Row(
-  //                   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-  //                   crossAxisAlignment: CrossAxisAlignment.center,
-  //                   children: <Widget>[
-  //                     Text("Item Total Price"),
-  //                     Text("250")
-  //                   ],
-  //                 )
-  //           ],
-  //         ),
-  //       ),
-  //     ),
-  //   );
-  // });
-  // }
+  
 
   @override
   _ProductCardNewState createState() => _ProductCardNewState();
@@ -137,6 +27,111 @@ class _ProductCardNewState extends State<ProductCardNew> {
   StoreProduct e;
   StoreProduct f;
 
+onClickProduct() {
+    showGeneralDialog(
+  context: context,
+  barrierDismissible: true,
+  barrierLabel:
+      MaterialLocalizations.of(context).modalBarrierDismissLabel,
+  barrierColor: Colors.black45,
+  transitionDuration: const Duration(milliseconds: 200),
+  pageBuilder: (BuildContext buildContext, Animation animation,
+      Animation secondaryAnimation) {
+    return Material(
+      type: MaterialType.transparency,
+      child: Center(
+        child: Container(
+          width: MediaQuery.of(context).size.width - 50,
+          height: MediaQuery.of(context).size.height - 300,
+          padding: EdgeInsets.all(20),
+          color: Colors.white,
+          child: Column(
+            children: [
+              RaisedButton(
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+                child: Text(
+                  "Save",
+                  style: TextStyle(color: Colors.white),
+                ),
+                color: const Color(0xFF1BC0C5),
+              ),
+              Text(
+                "Apple - Red Delicious",
+                style: TextStyle(
+                    color: ThemeColoursSeva().pallete2,
+                    fontSize: 25.0,
+                    fontWeight: FontWeight.w500),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Text("Rs 120 - 1 Kg",
+                  overflow: TextOverflow.clip,
+                  style: TextStyle(
+                      color: ThemeColoursSeva().pallete2,
+                      fontSize: 25.0,
+                      fontWeight: FontWeight.w500)),
+              SizedBox(height: 10),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  CachedNetworkImage(
+                      width: 200,
+                      height: 140,
+                      imageUrl:
+                          "https://storepictures.theonestop.co.in/products/pineapple.png"),
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    // crossAxisAlignment: CrossAxisAlignment.,
+                    children: <Widget>[
+                      Text(
+                        "200 Gms",
+                        style: TextStyle(
+                            color: ThemeColoursSeva().pallete2,
+                            fontSize: 20.0,
+                            fontWeight: FontWeight.w500),
+                      ),
+                      Text(
+                        "200 Gms",
+                        style: TextStyle(
+                            color: ThemeColoursSeva().pallete2,
+                            fontSize: 20.0,
+                            fontWeight: FontWeight.w500),
+                      ),
+                      Text(
+                        "200 Gms",
+                        style: TextStyle(
+                            color: ThemeColoursSeva().pallete2,
+                            fontSize: 20.0,
+                            fontWeight: FontWeight.w500),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Text(
+                  "Apples contain  no fat, sodium or cholestrol and are a good source"),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: <Widget>[
+                      Text("Item Total Price"),
+                      Text("250")
+                    ],
+                  )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+  );
+  }
   @override
   initState() {
     super.initState();
@@ -196,64 +191,67 @@ class _ProductCardNewState extends State<ProductCardNew> {
             child: FlipCard(
                 // key: cardKey,
                 flipOnTouch: this.widget.shopping ? true : false,
-                front: Container(
-                  width: 100.0,
-                  decoration: BoxDecoration(
-                    border: Border.all(
-                      color: ThemeColoursSeva().pallete3,
-                      width: 1.5,
+                front: GestureDetector(
+                onTap:()=> this.widget.shopping ? null:onClickProduct(),
+                                  child: Container(
+                    width: 100.0,
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                        color: ThemeColoursSeva().pallete3,
+                        width: 1.5,
+                      ),
+                      borderRadius: BorderRadius.circular(20.0),
                     ),
-                    borderRadius: BorderRadius.circular(20.0),
-                  ),
-                  child: Column(
-                    children: <Widget>[
-                      SizedBox(
-                        height: 15,
-                      ),
-                      Text(
-                        "apple",
-                        overflow: TextOverflow.clip,
-                        style: TextStyle(
-                            color: ThemeColoursSeva().pallete2,
-                            fontSize: 15.0,
-                            fontWeight: FontWeight.w500),
-                      ),
-                      SizedBox(height: 20),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        children: <Widget>[
-                          this.widget.shopping
-                              ? Expanded(child: Icon(Icons.edit))
-                              : Container(),
-                          ConstrainedBox(
-                            constraints:
-                                BoxConstraints(maxWidth: 67.3, maxHeight: 160),
-                            child: CachedNetworkImage(
-                                imageUrl:
-                                    "https://storepictures.theonestop.co.in/products/pineapple.png"),
-                          ),
-                          this.widget.shopping
-                              ? Expanded(
-                                  child: IconButton(
-                                      icon: Icon(Icons.delete),
-                                      onPressed: () {
-                                        print("something");
-                                      }),
-                                )
-                              : Container(),
-                        ],
-                      ),
-                      SizedBox(height: 20),
-                      Text("Rs 120 - 1 Kg",
+                    child: Column(
+                      children: <Widget>[
+                        SizedBox(
+                          height: 15,
+                        ),
+                        Text(
+                          "apple",
                           overflow: TextOverflow.clip,
                           style: TextStyle(
                               color: ThemeColoursSeva().pallete2,
                               fontSize: 15.0,
-                              fontWeight: FontWeight.w500)),
-                      SizedBox(
-                        height: 30,
-                      )
-                    ],
+                              fontWeight: FontWeight.w500),
+                        ),
+                        SizedBox(height: 20),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: <Widget>[
+                            this.widget.shopping
+                                ? Expanded(child: Icon(Icons.edit))
+                                : Container(),
+                            ConstrainedBox(
+                              constraints:
+                                  BoxConstraints(maxWidth: 67.3, maxHeight: 160),
+                              child: CachedNetworkImage(
+                                  imageUrl:
+                                      "https://storepictures.theonestop.co.in/products/pineapple.png"),
+                            ),
+                            this.widget.shopping
+                                ? Expanded(
+                                    child: IconButton(
+                                        icon: Icon(Icons.delete),
+                                        onPressed: () {
+                                          print("something");
+                                        }),
+                                  )
+                                : Container(),
+                          ],
+                        ),
+                        SizedBox(height: 20),
+                        Text("Rs 120 - 1 Kg",
+                            overflow: TextOverflow.clip,
+                            style: TextStyle(
+                                color: ThemeColoursSeva().pallete2,
+                                fontSize: 15.0,
+                                fontWeight: FontWeight.w500)),
+                        SizedBox(
+                          height: 30,
+                        )
+                      ],
+                    ),
                   ),
                 ),
                 back: this.widget.shopping
