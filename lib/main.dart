e0c6727df35216749aafaf8ff26b35080bf04d6a
Rahul_Vijay <vk.rahul318@gmail.com>
2020-07-31 12:34:29 +0530

    mainLanding

--- lib/main.dart
+++ lib/main.dart
@@ -54,7 +54,7 @@ class _SevaAppState extends State<SevaApp> {
       providers: [ChangeNotifierProvider(create: (context) => CartModel())],
       child: MaterialApp(
         debugShowCheckedModeBanner: false,
-        home: LoadingScreen(),
+        home: MainLandingScreen(),
         routes: {
           "/register": (context) => RegisterScreen(),
           "/login": (context) => LoginScreen(),
