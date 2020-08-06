88ffef70a9e5e762da70908c7b59114e4e28422a
Rahul_Vijay <vk.rahul318@gmail.com>
2020-08-06 15:24:09 +0530

    design done

--- lib/screens/common/AnimatedCard/modalContainer.dart
+++ lib/screens/common/AnimatedCard/modalContainer.dart
@@ -13,107 +13,136 @@ class AddItemModal extends StatefulWidget {
 class _AddItemModalState extends State<AddItemModal> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: MediaQuery.of(context).size.width - 50,
-      height: MediaQuery.of(context).size.height - 300,
-      padding: EdgeInsets.all(20),
-      color: Colors.white,
-      child: Column(
-        children: [
-          Text(
-            widget.product.name,
-            style: TextStyle(
-              color: ThemeColoursSeva().pallete2,
-              fontSize: 22.0,
-              fontWeight: FontWeight.w500,
-              decoration: TextDecoration.none,
+    return Material(
+      child: Container(
+        width: MediaQuery.of(context).size.width - 50,
+        height: MediaQuery.of(context).size.height - 300,
+        padding: EdgeInsets.all(20),
+        color: Colors.white,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Text(
+              widget.product.name,
+              style: TextStyle(
+                color: ThemeColoursSeva().dkGreen,
+                fontSize: 22.0,
+                fontWeight: FontWeight.w300,
+              ),
             ),
-          ),
-          SizedBox(
-            height: 10,
-          ),
-          Text(
-              "Rs ${widget.product.price} - ${widget.product.quantity.quantityValue} ${widget.product.quantity.quantityMetric}",
+            Text(
+              "Rs ${widget.product.price} for ${widget.product.quantity.quantityValue} ${widget.product.quantity.quantityMetric}",
               overflow: TextOverflow.clip,
               style: TextStyle(
-                color: ThemeColoursSeva().pallete2,
+                color: ThemeColoursSeva().dkGreen,
                 fontSize: 20.0,
                 fontWeight: FontWeight.w500,
-                decoration: TextDecoration.none,
-              )),
-          SizedBox(height: 10),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: <Widget>[
-              CachedNetworkImage(
-                  width: 100, height: 80, imageUrl: widget.product.pictureUrl),
-              Expanded(
-                child: Column(
-                  children: [
-                    SizedBox(
-                      height: 100,
-                      child: ListView.builder(
-                        // shrinkWrap: true,
-                        itemCount:
-                            widget.product.quantity.allowedQuantities.length,
-                        itemBuilder: (builder, i) {
-                          return Column(
-                            children: [
-                              Text(
-                                "${widget.product.quantity.allowedQuantities[i].value} ${widget.product.quantity.allowedQuantities[i].metric}",
-                                style: TextStyle(
-                                    decoration: TextDecoration.none,
-                                    color: ThemeColoursSeva().pallete2,
-                                    fontSize: 18.0,
-                                    fontWeight: FontWeight.w500),
-                              ),
-                              SizedBox(height: 7.0),
-                            ],
-                          );
-                        },
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                CachedNetworkImage(
+                    width: 90,
+                    height: 120,
+                    imageUrl: widget.product.pictureUrl),
+                SizedBox(width: 30.0),
+                Expanded(
+                  child: Column(
+                    children: [
+                      SizedBox(
+                        height: 145,
+                        child: ListView.builder(
+                          // shrinkWrap: true,
+                          itemCount:
+                              widget.product.quantity.allowedQuantities.length,
+                          itemBuilder: (builder, i) {
+                            return Column(
+                              children: [
+                                Row(
+                                  children: [
+                                    Text(
+                                      "${widget.product.quantity.allowedQuantities[i].value} ${widget.product.quantity.allowedQuantities[i].metric}",
+                                      style: TextStyle(
+                                          color: ThemeColoursSeva().dkGreen,
+                                          fontSize: 18.0,
+                                          fontWeight: FontWeight.w300),
+                                    ),
+                                    IconButton(
+                                      icon: Icon(Icons.remove),
+                                      onPressed: () {},
+                                    ),
+                                    Text("0"),
+                                    IconButton(
+                                      icon: Icon(Icons.add),
+                                      onPressed: () {},
+                                    ),
+                                  ],
+                                ),
+                              ],
+                            );
+                          },
+                        ),
                       ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
+              ],
+            ),
+            Text(
+              "Apples contain no fat, sodium or cholestrol and are a good source of fibre.",
+              style: TextStyle(
+                color: ThemeColoursSeva().dkGreen,
+                fontSize: 20.0,
+                fontWeight: FontWeight.w300,
               ),
-            ],
-          ),
-          SizedBox(
-            height: 10,
-          ),
-          Text(
-            "Apples contain  no fat, sodium or cholestrol and are a good source of fibre.",
-            style: TextStyle(decoration: TextDecoration.none, fontSize: 10),
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: <Widget>[
-              Text(
-                "Item Total Price",
-                style: TextStyle(decoration: TextDecoration.none, fontSize: 10),
-              ),
-              Text(
-                "Rs 250",
-                style: TextStyle(decoration: TextDecoration.none, fontSize: 10),
-              )
-            ],
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: <Widget>[
-              Text(
-                "Cart Total Price",
-                style: TextStyle(decoration: TextDecoration.none, fontSize: 10),
-              ),
-              Text(
-                "Rs 250",
-                style: TextStyle(decoration: TextDecoration.none, fontSize: 10),
-              )
-            ],
-          )
-        ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                Text(
+                  "Item Total Price",
+                  style: TextStyle(
+                    color: ThemeColoursSeva().dkGreen,
+                    fontSize: 20.0,
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+                Text(
+                  "Rs 250",
+                  style: TextStyle(
+                    color: ThemeColoursSeva().dkGreen,
+                    fontSize: 20.0,
+                    fontWeight: FontWeight.w500,
+                  ),
+                )
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                Text(
+                  "Cart Total Price",
+                  style: TextStyle(
+                    color: ThemeColoursSeva().dkGreen,
+                    fontSize: 20.0,
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+                Text(
+                  "Rs 250",
+                  style: TextStyle(
+                    color: ThemeColoursSeva().dkGreen,
+                    fontSize: 20.0,
+                    fontWeight: FontWeight.w500,
+                  ),
+                )
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
