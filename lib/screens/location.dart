bc2b6e69464bea13ae8a44934e39c977a2bf2715
Vikas Prashar <vikasprashar99@gmail.com>
2020-09-08 11:34:37 +0530

    removed maps gecoding from this branch

--- lib/screens/location.dart
+++ lib/screens/location.dart
@@ -1,10 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:geocoding/geocoding.dart' as geoLoc;
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 import 'package:location/location.dart';
 import 'package:mvp/constants/themeColours.dart';
-import 'package:mvp/screens/common/inputTextField.dart';
 import 'package:mvp/screens/errors/locationService.dart';
 import 'package:mvp/screens/userProfile.dart';
 
@@ -22,14 +20,13 @@ class _GoogleLocationScreenState extends State<GoogleLocationScreen> {
 
   final LatLng _center = const LatLng(12.9716, 77.5946);
   LatLng _userPosition;
-  Map<MarkerId, Marker> _markers = <MarkerId, Marker>{};
+  Set<Marker> _markers = {};
   bool _showActionBtn;
   Location location = new Location();
   bool _serviceEnabled;
   PermissionStatus _permissionGranted;
   LocationData _locationData;
-  int _markerIdCounter = 0;
-  String _markerAddress = "";
+
   @override
   void initState() {
     super.initState();
@@ -69,35 +66,22 @@ class _GoogleLocationScreenState extends State<GoogleLocationScreen> {
     });
   }
 
-// for changing marker pos
-  String _markerIdVal({bool increment = false}) {
-    String val = 'marker_id_$_markerIdCounter';
-    if (increment) _markerIdCounter++;
-    return val;
-  }
-
   void getCurrentLocation(ld) async {
     LatLng coords = LatLng(ld.latitude, ld.longitude);
-    MarkerId markerId = MarkerId(_markerIdVal());
-    LatLng position = coords;
-    Marker marker = Marker(
-      markerId: markerId,
-      position: position,
-      draggable: false,
-    );
+    Marker mk1 = Marker(
+        markerId: MarkerId('current'),
+        position: coords,
+        draggable: true,
+        onDragEnd: ((value) {
+          setState(() {
+            coords = LatLng(value.latitude, value.longitude);
+            _userPosition = coords;
+          });
+        }));
     setState(() {
-      _markers[markerId] = marker;
-    });
-
-    Future.delayed(Duration(seconds: 1), () async {
-      mapController.animateCamera(
-        CameraUpdate.newCameraPosition(
-          CameraPosition(
-            target: position,
-            zoom: 17.0,
-          ),
-        ),
-      );
+      mapController.animateCamera(CameraUpdate.newCameraPosition(
+          CameraPosition(target: coords, zoom: 18.0)));
+      _markers.add(mk1);
       _userPosition = coords;
       _showActionBtn = true;
     });
@@ -109,113 +93,57 @@ class _GoogleLocationScreenState extends State<GoogleLocationScreen> {
 
   void _onMapCreated(GoogleMapController controller) {
     mapController = controller;
+    // _encodeLocation();
   }
 
   _showFloatingActionButton() {
     if (_showActionBtn == true) {
-      return Padding(
-        padding: const EdgeInsets.all(40),
-        child: FloatingActionButton.extended(
-          backgroundColor: ThemeColoursSeva().dkGreen,
-          onPressed: () {
-            // _searchControl.clear();
-            // Navigator.push(
-            //     context,
-            //     MaterialPageRoute(
-            //         builder: (context) => UserProfileScreen(
-            //               coords: _userPosition,
-            //               userEmail: widget.userEmail,
-            //             )));
-          },
-          label: Text("Set as Delivery Address"),
-          icon: Icon(Icons.home),
-        ),
+      return FloatingActionButton.extended(
+        backgroundColor: ThemeColoursSeva().dkGreen,
+        onPressed: () {
+          _searchControl.clear();
+          Navigator.push(
+              context,
+              MaterialPageRoute(
+                  builder: (context) => UserProfileScreen(
+                        coords: _userPosition,
+                        userEmail: widget.userEmail,
+                      )));
+        },
+        label: Text("Set as Delivery Address"),
+        icon: Icon(Icons.home),
       );
     } else {
       return Container();
     }
   }
 
-  onMapsMove(position) async {
-    if (_markers.length > 0) {
-      MarkerId markerId = MarkerId(_markerIdVal());
-      Marker marker = _markers[markerId];
-      Marker updatedMarker = marker.copyWith(
-        positionParam: position.target,
-      );
-
-      setState(() {
-        _markers[markerId] = updatedMarker;
-      });
-    }
-    List<geoLoc.Placemark> placemarks = await geoLoc.placemarkFromCoordinates(
-        position.target.latitude, position.target.longitude);
-    geoLoc.Placemark placeMark = placemarks[0];
-    String name = placeMark.name;
-    String subLocality = placeMark.subLocality;
-    String locality = placeMark.locality;
-    String administrativeArea = placeMark.administrativeArea;
-    String postalCode = placeMark.postalCode;
-    String country = placeMark.country;
-    String address =
-        "$name, $subLocality, $locality, $administrativeArea $postalCode, $country";
-    this.setState(() {
-      _markerAddress = address;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       resizeToAvoidBottomPadding: false,
-      body: SafeArea(
-        child: Column(
-          children: <Widget>[
-            Expanded(
-              child: GoogleMap(
-                myLocationEnabled: true,
-                zoomGesturesEnabled: true,
-                scrollGesturesEnabled: true,
-                rotateGesturesEnabled: true,
-                zoomControlsEnabled: false,
-                myLocationButtonEnabled: true,
-                onMapCreated: _onMapCreated,
-                initialCameraPosition:
-                    CameraPosition(target: _center, zoom: 15.0),
-                markers: Set<Marker>.of(_markers.values),
-                onTap: (pos) {},
-                onCameraMove: (CameraPosition position)  {
-                  onMapsMove(position);
-                },
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(20),
-              child: Text(_markerAddress),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(10.0),
-              child: InputTextField(
-                  lt: "House No/Flat No:",
-                  // eC: TextEditingController()..text = _markerAddress
-                  ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(10.0),
-              child: InputTextField(
-                lt: "Landmark:",
-                // eC: TextEditingController()..text = _markerAddress
-              ),
-            ),
-            Container(
-              child: _showFloatingActionButton(),
-            ),
-            SizedBox(
-              height: MediaQuery.of(context).viewInsets.bottom,
-            ),
-          ],
-        ),
+      body: Stack(
+        children: <Widget>[
+          GoogleMap(
+            onMapCreated: _onMapCreated,
+            initialCameraPosition: CameraPosition(target: _center, zoom: 15.0),
+            markers: Set.from(_markers),
+            onTap: (pos) {
+              // _showActionBtn = true;
+              // _userPosition = pos;
+              // Marker mk1 = Marker(
+              //   markerId: MarkerId('1'),
+              //   position: pos,
+              // );
+              // setState(() {
+              //   _markers.add(mk1);
+              // });
+            },
+          ),
+        ],
       ),
+      floatingActionButton: _showFloatingActionButton(),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
     );
   }
 }
