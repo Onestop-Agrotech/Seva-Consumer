9b485baf72546dc46a4e235e6b2340a4a45c0e2d
Rahul_Vijay <vk.rahul318@gmail.com>
2020-09-10 17:58:51 +0530

    firebase ios

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,6 +1,7 @@
 import UIKit
 import Flutter
 import GoogleMaps
+import Firebase
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -11,6 +12,7 @@ import GoogleMaps
     var flutter_native_splash = 1
     UIApplication.shared.isStatusBarHidden = false
     GMSServices.provideAPIKey("AIzaSyDsyD8-n3nXwZuyE4nOu9VEWUb9iJJYbD4")
+    FirebaseApp.configure()
 
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
