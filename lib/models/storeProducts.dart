558f6d52a9c9605f7e6c1fea10f53eca9d251e17
Vikas Prashar <vikasprashar99@gmail.com>
2020-09-10 22:44:29 +0530

    modal

--- lib/models/storeProducts.dart
+++ lib/models/storeProducts.dart
@@ -8,55 +8,84 @@ String storeProductTojsonModel(List<StoreProduct> data) =>
     json.encode(List<dynamic>.from(data.map((x) => x.toJson())));
 
 class StoreProduct {
-  StoreProduct(
-      {this.quantity,
-      this.id,
-      this.name,
-      this.type,
-      this.uniqueId,
-      this.description,
-      this.price,
-      this.funFact = "",
-      this.pictureUrl,
-      this.totalPrice = 0,
-      this.totalQuantity = 0,
-      this.outOfStock});
-
   String id;
   String name;
   String type;
   String uniqueId;
   String description;
-  int price;
-  String pictureUrl;
-  String funFact;
+  String pictureURL;
   double totalPrice;
   double totalQuantity;
-  Quantity quantity;
-  bool outOfStock;
+  List<Details> details;
+  int iV;
+
+  StoreProduct(
+      {this.id,
+      this.name,
+      this.type,
+      this.uniqueId,
+      this.description,
+      this.pictureURL,
+      this.details,
+      this.totalPrice = 0,
+      this.totalQuantity = 0,
+      this.iV});
 
   factory StoreProduct.fromJson(Map<String, dynamic> json) => StoreProduct(
-        id: json["_id"],
-        name: json["name"],
-        type: json["type"],
-        uniqueId: json["uniqueId"],
-        description: json["description"],
-        price: json["price"],
-        funFact: json["funFact"],
-        pictureUrl: json["pictureURL"],
-        outOfStock: json["outOfStock"],
-        quantity: Quantity.fromJson(json["quantity"]),
+      id: json['_id'],
+      name: json['name'],
+      type: json['type'],
+      uniqueId: json['uniqueId'],
+      description: json['description'],
+      pictureURL: json['pictureURL'],
+      details:
+          List<Details>.from(json["details"].map((x) => Details.fromJson(x))),
+      iV: json['__v']);
+
+  Map<String, dynamic> toJson() => {
+        '_id': id,
+        'name': name,
+        'type': type,
+        'uniqueId': uniqueId,
+        'description': description,
+        'pictureURL': pictureURL,
+        'details': List<dynamic>.from(details.map((x) => x.toJson())),
+        '__v': iV
+      };
+}
+
+class Details {
+  Quantity quantity;
+  String id;
+  String hubid;
+  int price;
+  bool outOfStock;
+  bool bestseller;
+
+  Details(
+      {this.quantity,
+      this.id,
+      this.hubid,
+      this.price,
+      this.outOfStock,
+      this.bestseller});
+
+  factory Details.fromJson(Map<String, dynamic> json) => Details(
+        quantity: Quantity.fromJson(json['quantity']),
+        id: json['_id'],
+        hubid: json['hubid'],
+        price: json['price'],
+        outOfStock: json['outOfStock'],
+        bestseller: json['bestseller'],
       );
 
   Map<String, dynamic> toJson() => {
-        "_id": id,
-        "name": name,
-        "type": type,
-        "uniqueId": uniqueId,
-        "description": description,
-        "price": price,
-        "pictureURL": pictureUrl,
-        "quantity": quantity.toJson(),
+        'quantity': quantity.toJson(),
+        '_id': id,
+        'hubid': hubid,
+        'price': price,
+        'outOfStock': outOfStock,
+        'bestseller': bestseller,
       };
 }
 
@@ -81,16 +110,18 @@ class Quantity {
 }
 
 class AllowedQuantity {
-  AllowedQuantity({this.value, this.metric, this.qty = 0});
+  String id;
   int value;
   String metric;
   int qty;
 
+  AllowedQuantity({this.id, this.value, this.metric,this.qty = 0});
+
   factory AllowedQuantity.fromJson(Map<String, dynamic> json) =>
       AllowedQuantity(
-        value: json["quantityValue"],
-        metric: json["quantityMetric"],
-      );
+          id: json['_id'],
+          value: json['quantityValue'],
+          metric: json['quantityMetric']);
 
   Map<String, dynamic> toJson() => {
         "quantityValue": value,
