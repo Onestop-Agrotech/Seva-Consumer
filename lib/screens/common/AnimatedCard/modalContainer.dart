1f401c5f3fe82b7a91f1901c5a463afbe0773a89
Vikas Prashar <vikasprashar99@gmail.com>
2020-09-16 23:07:48 +0530

    fix lint error

--- lib/screens/common/AnimatedCard/modalContainer.dart
+++ lib/screens/common/AnimatedCard/modalContainer.dart
@@ -97,7 +97,7 @@ class _AddItemModalState extends State<AddItemModal> {
     double totalQ = 0.0;
     newCart.items.forEach((e) {
       if (e.id == widget.product.id) {
-        totalQ =e.totalQuantity;
+        totalQ = e.totalQuantity;
       }
     });
 
