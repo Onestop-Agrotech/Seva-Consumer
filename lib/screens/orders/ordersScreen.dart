d748a780950e451689e16e7789bf85915c27ebc8
Rahul_Vijay <vk.rahul318@gmail.com>
2020-09-18 15:52:09 +0530

    remove unused import

--- lib/screens/orders/ordersScreen.dart
+++ lib/screens/orders/ordersScreen.dart
@@ -10,7 +10,6 @@
 
 import 'dart:async';
 import 'package:flutter/material.dart';
-import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 import 'dart:convert';
 import 'package:mvp/classes/storage_sharedPrefs.dart';
 import 'package:mvp/constants/apiCalls.dart';
