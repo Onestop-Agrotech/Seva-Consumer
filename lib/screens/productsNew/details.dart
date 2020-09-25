fccd9aa584522c06192a7540230c42c3d77883dd
Vikas Prashar <vikasprashar99@gmail.com>
2020-09-25 21:50:44 +0530

    details ui updated

--- lib/screens/productsNew/details.dart
+++ lib/screens/productsNew/details.dart
@@ -1,6 +1,5 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
-import 'package:mvp/constants/themeColours.dart';
 import 'package:mvp/models/newCart.dart';
 import 'package:mvp/models/storeProducts.dart';
 import 'package:mvp/sizeconfig/sizeconfig.dart';
@@ -14,7 +13,6 @@ class ProductDetails extends StatefulWidget {
 }
 
 class _ProductDetailsState extends State<ProductDetails> {
-  int i;
   // shows/edit the total quantity of the product
   Text _showTotalItemQty(newCart) {
     double totalQ = 0.0;
@@ -28,7 +26,10 @@ class _ProductDetailsState extends State<ProductDetails> {
       totalQ != 0.0
           ? "${totalQ.toStringAsFixed(2)} ${widget.p.details[0].quantity.quantityMetric}"
           : "0",
-      style: TextStyle(fontWeight: FontWeight.bold, fontSize: 15),
+      style: TextStyle(
+          fontWeight: FontWeight.bold,
+          fontSize: SizeConfig.textMultiplier * 2.1,
+          color: Colors.blueGrey),
     );
   }
 
@@ -42,7 +43,10 @@ class _ProductDetailsState extends State<ProductDetails> {
     });
     return Text(
       "Rs $price",
-      style: TextStyle(fontWeight: FontWeight.bold, fontSize: 15),
+      style: TextStyle(
+          fontWeight: FontWeight.bold,
+          fontSize: SizeConfig.textMultiplier * 2.1,
+          color: Colors.blueGrey),
     );
   }
 
@@ -52,8 +56,22 @@ class _ProductDetailsState extends State<ProductDetails> {
     price = newCart.getCartTotalPrice();
     return Text(
       "Rs $price",
-      style: TextStyle(fontWeight: FontWeight.bold, fontSize: 15),
+      style: TextStyle(
+          fontWeight: FontWeight.bold,
+          fontSize: SizeConfig.textMultiplier * 2.1,
+          color: Colors.blueGrey),
     );
+  }
+
+  // shows/edit the quantity of the product
+  Text _showQ(newCart, item, index) {
+    int qty = 0;
+    newCart.items.forEach((e) {
+      if (e.id == item.id) {
+        qty = e.details[0].quantity.allowedQuantities[index].qty;
+      }
+    });
+    return Text("$qty");
   }
 
   void helper(int index, newCart, bool addToCart) {
@@ -102,9 +120,19 @@ class _ProductDetailsState extends State<ProductDetails> {
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         children: [
-          Text(widget.p.name),
           Text(
-              "Rs ${widget.p.details[0].price} per ${widget.p.details[0].quantity.quantityMetric}")
+            widget.p.name,
+            style: TextStyle(
+                fontSize: SizeConfig.textMultiplier * 2.5,
+                fontWeight: FontWeight.bold,
+                color: Colors.black54),
+          ),
+          Text(
+              "Rs ${widget.p.details[0].price} per ${widget.p.details[0].quantity.quantityMetric}",
+              style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: SizeConfig.textMultiplier * 2.5,
+                  color: Colors.blueGrey))
         ],
       ),
     );
@@ -120,19 +148,47 @@ class _ProductDetailsState extends State<ProductDetails> {
     );
   }
 
-  Widget getQuantities(String quantity, double height) {
+  Widget getQuantities(String quantity, double height, newCart, index) {
     return Padding(
       padding: const EdgeInsets.only(top: 20.0, bottom: 20.0),
       child: Column(
         children: [
           Text(
             quantity,
-            style: TextStyle(fontSize: 24.0, color: Colors.deepPurpleAccent),
+            style: TextStyle(
+                fontSize: SizeConfig.textMultiplier * 2.5,
+                color: Colors.black54),
           ),
           SizedBox(height: height * 0.009),
           Row(
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [Icon(Icons.remove), Text("0"), Icon(Icons.add)],
+            children: [
+              GestureDetector(
+                onTap: () {
+                  helper(index, newCart, false);
+                },
+                child: Container(
+                    width: SizeConfig.widthMultiplier * 8,
+                    height: SizeConfig.widthMultiplier * 8,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(20),
+                        color: Colors.green),
+                    child: Icon(Icons.remove, color: Colors.white54)),
+              ),
+              _showQ(newCart, widget.p, index),
+              GestureDetector(
+                onTap: () {
+                  helper(index, newCart, true);
+                },
+                child: Container(
+                    width: SizeConfig.widthMultiplier * 8,
+                    height: SizeConfig.widthMultiplier * 8,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(20),
+                        color: Colors.green),
+                    child: Icon(Icons.add, color: Colors.white54)),
+              )
+            ],
           )
         ],
       ),
@@ -150,69 +206,113 @@ class _ProductDetailsState extends State<ProductDetails> {
         appBar: AppBar(
           title: Text("Product name"),
         ),
-        body: Container(
-          height: height * 0.88,
-          width: width,
-          child: Column(
-            children: [
-              getImage(height, width),
-              getNameAndPrice(height, width),
-              Container(
-                height: height * 0.4,
-                width: width,
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    Container(
-                      width: width * 0.33,
-                      height: height * 0.4,
-                      child: Column(
-                        children: [
-                          Expanded(
-                            child: ListView.builder(
-                              itemCount: widget.p.details[0].quantity
-                                  .allowedQuantities.length,
-                              itemBuilder: (context, index) {
-                                return getQuantities(
-                                    "${widget.p.details[0].quantity.allowedQuantities[index].value} ${widget.p.details[0].quantity.allowedQuantities[index].metric}",
-                                    height);
-                              },
-                            ),
-                          )
-                        ],
+        body: Consumer<NewCartModel>(builder: (context, newCart, child) {
+          return Container(
+            height: height * 0.88,
+            width: width,
+            child: ListView(
+              children: [
+                getImage(height, width),
+                getNameAndPrice(height, width),
+                Container(
+                  height: height * 0.4,
+                  width: width,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Container(
+                        width: width * 0.33,
+                        height: height * 0.4,
+                        child: Column(
+                          children: [
+                            Expanded(
+                              child: ListView.builder(
+                                itemCount: widget.p.details[0].quantity
+                                    .allowedQuantities.length,
+                                itemBuilder: (context, index) {
+                                  return getQuantities(
+                                      "${widget.p.details[0].quantity.allowedQuantities[index].value} ${widget.p.details[0].quantity.allowedQuantities[index].metric}",
+                                      height,
+                                      newCart,
+                                      index);
+                                },
+                              ),
+                            )
+                          ],
+                        ),
                       ),
-                    ),
-                    Column(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: [
-                        Column(
-                          children: [Text("Item Total Quantity"), Text("2kgs")],
-                        ),
-                        Column(
-                          children: [Text("Item Total Quantity"), Text("2kgs")],
-                        ),
-                        Column(
-                          children: [Text("Item Total Quantity"), Text("2kgs")],
-                        ),
-                      ],
-                    )
-                  ],
-                ),
-              ),
-              SizedBox(height: height * 0.035),
-              Container(
-                height: height * 0.1,
-                width: width,
-                child: Padding(
-                  padding: const EdgeInsets.only(left: 10.0),
-                  child: Text(
-                    "A fun fact or some kind of product description over here, which may include product grade, quality etc.",
-                    style: TextStyle(fontSize: 20.0, color: Colors.blueGrey),
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                "Item Total Quantity",
+                                style: TextStyle(
+                                    fontSize: SizeConfig.textMultiplier * 2.1,
+                                    color: Colors.black54),
+                              ),
+                              SizedBox(
+                                height: 10,
+                              ),
+                              _showTotalItemQty(newCart)
+                            ],
+                          ),
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                "Item Total Price",
+                                style: TextStyle(
+                                    fontSize: SizeConfig.textMultiplier * 2.1,
+                                    color: Colors.black54),
+                              ),
+                              SizedBox(
+                                height: 10,
+                              ),
+                              _showItemTotalPrice(newCart)
+                            ],
+                          ),
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                "Cart Total Price",
+                                style: TextStyle(
+                                    fontSize: SizeConfig.textMultiplier * 2.1,
+                                    color: Colors.black54),
+                              ),
+                              SizedBox(
+                                height: 10,
+                              ),
+                              _showTotalCartPrice(newCart)
+                            ],
+                          ),
+                        ],
+                      )
+                    ],
                   ),
                 ),
-              ),
-            ],
-          ),
-        ));
+                SizedBox(height: height * 0.035),
+                Container(
+                  height: height * 0.1,
+                  width: width,
+                  child: Padding(
+                    padding: const EdgeInsets.only(left: 20.0),
+                    child: Text(
+                      "A fun fact or some kind of product description over here, which may include product grade, quality etc.",
+                      style: TextStyle(
+                          fontSize: SizeConfig.textMultiplier * 2.5,
+                          color: Colors.black54,
+                          fontWeight: FontWeight.w500),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          );
+        }));
   }
 }
