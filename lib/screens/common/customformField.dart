697e3bcef8f9a95cf448136366c852bf47b6f5fa
Vikas Prashar <vikasprashar99@gmail.com>
2020-10-11 22:40:56 +0530

    added form field with proper error messages

--- lib/screens/common/customformField.dart
+++ lib/screens/common/customformField.dart
@@ -23,49 +23,48 @@ customTextField({
       ),
       Padding(
         padding: const EdgeInsets.symmetric(horizontal: 30.0),
-        child: Container(
-          height: 50.0,
-          width: double.infinity,
-          decoration: BoxDecoration(
-              color: Color(0xffebedf0),
-              borderRadius: BorderRadius.circular(20.0)),
-          child: Container(
-              width: 270.0,
-              child: Padding(
-                padding: const EdgeInsets.only(left: 20.0),
-                child: TextFormField(
-                  keyboardType: textInputType,
-                  controller: controller,
-                  decoration: InputDecoration(
-                    contentPadding: new EdgeInsets.symmetric(
-                      vertical: 1 * SizeConfig.textMultiplier,
-                      horizontal: 10.0,
-                    ),
-                    border: InputBorder.none,
-                    focusedBorder: InputBorder.none,
-                    enabledBorder: InputBorder.none,
-                    errorBorder: InputBorder.none,
-                    disabledBorder: InputBorder.none,
-                  ),
-                  validator: (String str) {
-                    if (str.isEmpty) {
-                      return "This Field cannot be empty!";
-                    } else if (str.isNotEmpty) {
-                      if (textInputType == TextInputType.emailAddress) {
-                        if (RegExp(r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+")
-                                .hasMatch(str) ==
-                            false) return "Not a valid email!";
-                      } else if (textInputType == TextInputType.number) {
-                        if (RegExp(r"^[0-9]{10}$").hasMatch(str) == false) {
-                          return "Not a valid mobile!";
-                        }
-                      }
-                    }
-                    return null;
-                  },
-                  maxLength: labelText == "Mobile" ? 10 : null,
-                ),
-              )),
+        child: TextFormField(
+          keyboardType: textInputType,
+          controller: controller,
+          decoration: InputDecoration(
+            filled: true,
+            // fillColor: ThemeColoursSeva().black,
+            contentPadding:
+                const EdgeInsets.only(left: 14.0, bottom: 8.0, top: 8.0),
+            focusedBorder: OutlineInputBorder(
+              borderSide: BorderSide(color: Colors.white),
+              borderRadius: BorderRadius.circular(25.7),
+            ),
+            enabledBorder: OutlineInputBorder(
+              borderSide: BorderSide(color: Colors.white),
+              borderRadius: BorderRadius.circular(25.7),
+            ),
+            errorBorder: OutlineInputBorder(
+              borderSide: BorderSide(color: Colors.white),
+              borderRadius: BorderRadius.circular(25.7),
+            ),
+            focusedErrorBorder: OutlineInputBorder(
+              borderSide: BorderSide(color: Colors.white),
+              borderRadius: BorderRadius.circular(25.7),
+            ),
+          ),
+          validator: (String str) {
+            if (str.isEmpty) {
+              return "This Field cannot be empty!";
+            } else if (str.isNotEmpty) {
+              if (textInputType == TextInputType.emailAddress) {
+                if (RegExp(r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+")
+                        .hasMatch(str) ==
+                    false) return "Not a valid email!";
+              } else if (textInputType == TextInputType.number) {
+                if (RegExp(r"^[0-9]{10}$").hasMatch(str) == false) {
+                  return "Not a valid mobile!";
+                }
+              }
+            }
+            return null;
+          },
+          maxLength: labelText == "Mobile" ? 10 : null,
         ),
       ),
     ],
