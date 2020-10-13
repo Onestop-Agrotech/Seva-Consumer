e412d255d2410e55a64a9485507e6b0cd42c5131
Rajat012k <rajatdav2000@gmail.com>
2020-10-13 22:54:39 +0530

    routing

---
+++ lib/screens/landing/common/routes.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/cupertino.dart';
+import 'package:mvp/screens/productsNew/newUI.dart';
+
+class Routes {
+  // common function for routing
+  static void routeto(Widget screen, context, index) {
+    Navigator.of(context)
+        .push(CupertinoPageRoute<Null>(builder: (BuildContext context) {
+      return ProductsUINew(tagFromMain: screen.index);
+    }));
+  }
+}
