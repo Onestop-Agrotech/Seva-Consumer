38fadfc3481a093afaabd4d39ec0fea6cf7eb457
Rahul_Vijay <vk.rahul318@gmail.com>
2020-10-26 11:32:29 +0530

    class setup

--- lib/models/categories.dart
+++ lib/models/categories.dart
@@ -1,29 +1,40 @@
 import 'dart:convert';
+import 'dart:ui';
 
-List<CategoryJSON> jsonToCategory(String str) => List<CategoryJSON>.from(json.decode(str).map((x) => CategoryJSON.fromJson(x)));
+import 'package:flutter/material.dart';
+import 'package:mvp/constants/themeColours.dart';
 
-String categoryToJson(List<CategoryJSON> data) => json.encode(List<dynamic>.from(data.map((x) => x.toJson())));
+List<CategoryJSON> jsonToCategory(String str) => List<CategoryJSON>.from(
+    json.decode(str).map((x) => CategoryJSON.fromJson(x)));
+
+String categoryToJson(List<CategoryJSON> data) =>
+    json.encode(List<dynamic>.from(data.map((x) => x.toJson())));
 
 class CategoryJSON {
-    CategoryJSON({
-        this.name,
-        this.categoryName,
-        this.hasData,
-    });
+  CategoryJSON(
+      {this.name,
+      this.categoryName,
+      this.hasData,
+      this.backgroundColor = Colors.white,
+      this.textColor}) {
+    this.textColor = ThemeColoursSeva().pallete1;
+  }
 
-    String name;
-    String categoryName;
-    bool hasData;
+  String name;
+  String categoryName;
+  bool hasData;
+  Color backgroundColor;
+  Color textColor;
 
-    factory CategoryJSON.fromJson(Map<String, dynamic> json) => CategoryJSON(
+  factory CategoryJSON.fromJson(Map<String, dynamic> json) => CategoryJSON(
         name: json["name"],
         categoryName: json["categoryName"],
         hasData: json["hasData"],
-    );
+      );
 
-    Map<String, dynamic> toJson() => {
+  Map<String, dynamic> toJson() => {
         "name": name,
         "categoryName": categoryName,
         "hasData": hasData,
-    };
+      };
 }
