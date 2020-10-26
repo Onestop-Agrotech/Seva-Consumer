33854dadbfeadbe64790c059fd02aaa932740c2c
Vikas Prashar <vikasprashar99@gmail.com>
2020-10-26 20:12:30 +0530

    location route

--- lib/screens/profile.dart
+++ lib/screens/profile.dart
@@ -1,6 +1,13 @@
+import 'dart:convert';
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:mvp/classes/prefrenses.dart';
+import 'package:mvp/constants/apiCalls.dart';
 import 'package:mvp/constants/themeColours.dart';
 import 'package:mvp/screens/common/cartIcon.dart';
+import 'package:mvp/screens/location.dart';
+import 'package:http/http.dart' as http;
 
 class Profile extends StatefulWidget {
   @override
@@ -8,6 +15,98 @@ class Profile extends StatefulWidget {
 }
 
 class _ProfileState extends State<Profile> {
+  String _email;
+
+  //To get the address of the user address on clicking the
+  // location icon
+  Future<String> _fetchUserAddress() async {
+    final p = await Preferences.getInstance();
+    String token = await p.getData("token");
+    String id = await p.getData("id");
+    Map<String, String> requestHeaders = {'x-auth-token': token};
+    String url = APIService.getAddressAPI + "$id";
+    var response = await http.get(url, headers: requestHeaders);
+    if (response.statusCode == 200) {
+      // got address
+      _email = json.decode(response.body)["email"];
+      return (json.decode(response.body)["address"]);
+    } else {
+      throw Exception('something is wrong');
+    }
+  }
+
+  //This function shows the user's address in a dialog box
+  // and the user can edit the address from their also
+  _showLocation(context) {
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.all(Radius.circular(20.0))),
+          title: Text(
+            "Delivery Address:",
+            style: TextStyle(
+                fontSize: 17.0,
+                color: Colors.black,
+                fontWeight: FontWeight.w500),
+          ),
+          content: FutureBuilder(
+              future: _fetchUserAddress(),
+              builder: (context, data) {
+                if (data.hasData) {
+                  return StatefulBuilder(builder: (context, setState) {
+                    return Container(
+                      height: 120.0,
+                      width: double.infinity,
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          SizedBox(height: 10.0),
+                          Container(
+                            width: MediaQuery.of(context).size.width * 0.6,
+                            child: Text(
+                              data.data,
+                              overflow: TextOverflow.clip,
+                            ),
+                          ),
+                          SizedBox(height: 30.0),
+                        ],
+                      ),
+                    );
+                  });
+                } else
+                  return Container(child: Text("Loading Address ..."));
+              }),
+          actions: <Widget>[
+            FutureBuilder(
+              future: _fetchUserAddress(),
+              builder: (context, data) {
+                if (data.hasData) {
+                  return RaisedButton(
+                    onPressed: () {
+                      Navigator.pop(context);
+                      Navigator.of(context).push(CupertinoPageRoute<Null>(
+                          builder: (BuildContext context) {
+                        return GoogleLocationScreen(
+                          userEmail: _email,
+                        );
+                      }));
+                    },
+                    child: Text("Change"),
+                    color: ThemeColoursSeva().pallete1,
+                    textColor: Colors.white,
+                  );
+                } else
+                  return Container();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -112,7 +211,9 @@ class _ProfileState extends State<Profile> {
                         const SizedBox(width: 8),
                         TextButton(
                           child: const Text('VIEW MY LOCATION'),
-                          onPressed: () {/* ... */},
+                          onPressed: () {
+                            _showLocation(context);
+                          },
                         ),
                         const SizedBox(width: 8),
                       ],
