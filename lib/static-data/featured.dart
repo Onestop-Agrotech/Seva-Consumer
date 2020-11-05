33fd862dcd7c8f1a15d02c76ad6235aa78560d5e
Rahul_Vijay <vk.rahul318@gmail.com>
2020-11-05 14:06:08 +0530

    order change

--- lib/static-data/featured.dart
+++ lib/static-data/featured.dart
@@ -2,14 +2,14 @@ import 'package:mvp/models/featured.dart';
 
 final List<Featured> featuredArr = [
   Featured(
-      name: 'SuperFastDelivery',
-      imgURL:
-          "https://storepictures.theonestop.co.in/illustrations/SuperFastDelivery-2.png",
-      clickable: false),
-  Featured(
       name: 'SupportLocal',
       imgURL:
           "https://storepictures.theonestop.co.in/illustrations/SupportLocal.png",
+      clickable: false),
+  Featured(
+      name: 'SuperFastDelivery',
+      imgURL:
+          "https://storepictures.theonestop.co.in/illustrations/SuperFastDelivery-2.png",
       clickable: false),
   Featured(
       name: 'ReferAFriend',
