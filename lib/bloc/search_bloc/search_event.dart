c524bb6e24c58c69b49b7d5560c784a2a08943e3
Rahul_Vijay <vk.rahul318@gmail.com>
2020-11-09 12:07:50 +0530

    search event

--- lib/bloc/search_bloc/search_event.dart
+++ lib/bloc/search_bloc/search_event.dart
@@ -2,3 +2,9 @@ part of 'search_bloc.dart';
 
 @immutable
 abstract class SearchEvent {}
+
+class SearchProduct extends SearchEvent {
+  final String name;
+
+  SearchProduct({@required this.name}) : assert(name != null);
+}
