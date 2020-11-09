3efc0e50259257a1d2344e144250d329b5aacb1e
Rahul_Vijay <vk.rahul318@gmail.com>
2020-11-09 12:05:37 +0530

    all search states

--- lib/bloc/search_bloc/search_bloc.dart
+++ lib/bloc/search_bloc/search_bloc.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 
 import 'package:bloc/bloc.dart';
 import 'package:meta/meta.dart';
+import 'package:mvp/models/storeProducts.dart';
 
 part 'search_event.dart';
 part 'search_state.dart';
--- lib/bloc/search_bloc/search_state.dart
+++ lib/bloc/search_bloc/search_state.dart
@@ -4,3 +4,17 @@ part of 'search_bloc.dart';
 abstract class SearchState {}
 
 class SearchInitial extends SearchState {}
+
+class SearchLoading extends SearchState {}
+
+class SearchLoaded extends SearchState {
+  final List<StoreProduct> searchResults;
+
+  SearchLoaded(this.searchResults);
+}
+
+class SearchError extends SearchState {
+  final String msg;
+
+  SearchError(this.msg);
+}
